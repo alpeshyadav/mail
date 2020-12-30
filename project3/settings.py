0491ea76465e8ff5b61b9c416c2a1be69a7d074f
alpesh yadav <alpeshyadav2518@gmail.com>
2020-12-30 18:37:45 +0530

    final commit

--- project3/settings.py
+++ project3/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mail',
         'USER': 'postgres',
-        'PASSWORD': 'Alpesh@4599',
+        'PASSWORD': '***',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
